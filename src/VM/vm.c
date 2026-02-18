6d75e7ff5686454c1cc48ae250410f3a0fa4dbab
zalos <satya123m4@gmail.com>
2026-02-18 14:43:09 +0530

    Feat: Complete Lexer

--- src/VM/vm.c
+++ src/VM/vm.c
@@ -90,7 +90,21 @@ static InterpretResult run() {
 }
 
 InterpretResult interpret(const char *source) {
-    compile(source);
+    Chunk chunk;
+    initChunk(&chunk);
 
+    if (!compile(source, &chunk)) {
+        freeChunk(&chunk);
+
+        return INTERPRET_COMPILE_ERROR;
+    }
+
+    // vm.chunk = &chunk;
+    // vm.ip = vm.chunk->code;
+
+    // InterpretResult result = run();
+
+    freeChunk(&chunk);
+    // return result;
     return INTERPRET_OK;
 }
